330fce475ae3ffc6b1f0d293b5388da50d7aa29c
Kacper <kacpietrzak@gmail.com>
2019-11-30 21:21:39 +0100

    Make submit.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000/user')
+const socket = io('http://localhost:1337/user')
 
 const joinGame = team => () => {
     socket.emit('join', {team})
@@ -11,4 +11,20 @@ $joinRed.addEventListener('click', joinGame('teamA'))
 const $joinBlue = document.getElementById('join-blue')
 $joinBlue.addEventListener('click', joinGame('teamB'))
 
+let selectedBorderId = null
+const $borderInputs = document.querySelectorAll('#border')
+$borderInputs.forEach($border => $border.addEventListener('click', (event) => {
+    if(selectedBorderId !== null) {
+        document.querySelector(`[borderId="${selectedBorderId}"]`).classList.remove('selected')
+    }
 
+    event.target.classList.add('selected')
+
+    const borderId = event.target.getAttribute('borderId')
+    selectedBorderId = borderId
+}))
+
+const $submitBtn = document.getElementById('submit-btn')
+$submitBtn.addEventListener('click', () => {
+    socket.emit('submit', selectedBorderId)
+})
