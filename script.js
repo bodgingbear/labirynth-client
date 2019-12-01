eec4b83027176bbbc748f6e9397b663c673b9f29
Luz4r <piorek.r@gmail.com>
2019-12-01 11:36:16 +0100

    make simple remove display

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const joinGame = team => () => {
     $teamName.textContent = teamStr.toUpperCase();
     $teamName.style.color = team === 'teamA' ? '#ff00ff' : '#fff000';
     document.querySelector('.container').prepend($teamName);
-    document.querySelector('#choose-screen').className = document.querySelector('#choose-screen').className.replace('hidden', '');
+    document.querySelector('#choose-screen').classList.remove('hidden');
     document.querySelector('#join-container').classList.add('hidden');
 }
 
